bfd6f858b40979f5b0f7960ec913af17f563aeda
ProfShibe <166961473+ProfShibe@users.noreply.github.com>
2024-12-27 18:04:10 -0600

    Add function calls from libraries to ensure they are working

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+#include "../libs/glm/glm.hpp"
+#include "../libs/imgui/imgui.h"
+#include <sol/sol.hpp>
+#include <sol/types.hpp>
 
 int main()
 {
+  // Test that everything works
+  SDL_Init(SDL_INIT_EVERYTHING);
+  sol::state lua;
+  lua.open_libraries(sol::lib::base);
+
+  auto velocity = glm::vec2(5.0, -2.0);
+
   std::cout << "buh" << std::endl;
 
   return 0;
