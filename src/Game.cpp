8da76e7812ecea8574552c9849496cc5a22abcd1
ProfShibe <166961473+ProfShibe@users.noreply.github.com>
2024-12-28 17:08:31 -0600

    Add Game class skeleton. Game class handles main loop

---
+++ src/Game.cpp
@@ -0,0 +1,39 @@
+#include "Game.h"
+#include <iostream>
+
+Game::Game() {
+  std::cout << "Game Constructor Called" << std::endl;
+}
+
+Game::~Game() {
+  std::cout << "Game Destructor Called" << std::endl;
+}
+
+void Game::Initialize() {
+
+};
+
+void Game::Run() {
+  while (1) {
+    ProcessInput();
+    Update();
+    Render();
+  }
+
+};
+
+void Game::ProcessInput() {
+
+};
+
+void Game::Update() {
+
+};
+
+void Game::Render() {
+
+};
+
+void Game::Destroy() {
+
+};
