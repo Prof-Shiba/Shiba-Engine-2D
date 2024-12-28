f9c3c939f80d56ea4c4f4028908ed84e7bdd6e16
ProfShibe <166961473+ProfShibe@users.noreply.github.com>
2024-12-28 17:07:43 -0600

    Remove test code and header files. Add game obj

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,8 @@
-#include <iostream>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
-#include <SDL2/SDL_mixer.h>
-#include "../libs/glm/glm.hpp"
-#include "../libs/imgui/imgui.h"
-#include <sol/sol.hpp>
-#include <sol/types.hpp>
+#include "Game.h"
 
-int main()
+int main(int argc, char* argv[])
 {
-  // Test that everything works
-  SDL_Init(SDL_INIT_EVERYTHING);
-  sol::state lua;
-  lua.open_libraries(sol::lib::base);
-
-  auto velocity = glm::vec2(5.0, -2.0);
-
-  std::cout << "buh" << std::endl;
-
+  Game game;
+  
   return 0;
 }
