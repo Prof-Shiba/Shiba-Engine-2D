c4a16ef58d302b3f5d3bf9d16c7e39295b3d6727
ProfShibe <166961473+ProfShibe@users.noreply.github.com>
2025-01-05 13:37:42 -0600

    Add Log entry struct to hold all messages. Add warning messages to logger

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -4,6 +4,8 @@
 #include <iomanip>
 #include "./Logger.h"
 
+std::vector<LogEntry> Logger::all_messages;
+
 std::string get_formatted_time() {
   const auto now = std::chrono::system_clock::now();
   const auto current_time = std::chrono::system_clock::to_time_t(now);
@@ -15,11 +17,29 @@ std::string get_formatted_time() {
   return oss.str();
 }
 
-
 void Logger::Log(const std::string& message) {
-  std::cout << CONSOLE_COLOR_GREEN << "LOG: [" << get_formatted_time() << "] " << message << CONSOLE_RESET_COLOR << std::endl;
+  LogEntry log_entry;
+  log_entry.type = LOG_INFO;
+  log_entry.message = "LOG: [" + get_formatted_time() + "] " + message;
+  all_messages.push_back(log_entry);
+
+  std::cout << CONSOLE_COLOR_GREEN << log_entry.message << CONSOLE_RESET_COLOR << std::endl;
 }
 
 void Logger::Err(const std::string& message) {
-  std::cerr << CONSOLE_COLOR_RED << "ERROR: [" << get_formatted_time() << "] " << message << CONSOLE_RESET_COLOR << std::endl; 
+  LogEntry log_entry;
+  log_entry.type = LOG_ERROR;
+  log_entry.message = "ERROR: [" + get_formatted_time() + "] " + message;
+  all_messages.push_back(log_entry);
+
+  std::cerr << CONSOLE_COLOR_RED << log_entry.message << CONSOLE_RESET_COLOR << std::endl; 
+}
+
+void Logger::Warn(const std::string& message) {
+  LogEntry log_entry;
+  log_entry.type = LOG_WARNING;
+  log_entry.message = "WARNING: [" + get_formatted_time() + "] " + message;
+  all_messages.push_back(log_entry);
+
+  std::cout << CONSOLE_COLOR_YELLOW << log_entry.message << CONSOLE_RESET_COLOR << std::endl; 
 }
