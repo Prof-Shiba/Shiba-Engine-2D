8b55ad91e2934a9103ac4ed0fdbd6b4d48f483fb
ProfShibe <166961473+ProfShibe@users.noreply.github.com>
2025-01-09 15:20:37 -0600

    Add MAX_COMPONENTS bitset, and declarations for some ECS functions

--- src/ECS/ECS.cpp
+++ src/ECS/ECS.cpp
@@ -1,4 +1,5 @@
 #include "ECS.hpp"
 
-// TODO: Implement functions from ECS.hpp
+int Entity::get_entity_id() const { return entity_id; }
 
+int Component::get_component_id() const { return component_id; }
--- src/ECS/ECS.hpp
+++ src/ECS/ECS.hpp
@@ -1,40 +1,72 @@
 #pragma once
+
+#include <cstdint>
+#include <bitset>
+#include <vector>
+
+///////////////////////////////////////////////////////////////
+// bitset tracks which components an entity has
+// also helps track which entities a system is interested in
+///////////////////////////////////////////////////////////////
+const uint8_t MAX_COMPONENTS = 32;
+typedef std::bitset<MAX_COMPONENTS> Signature;
+
+///////////////////////////////////////////////////////////////
+// Components store pure data that can be manipulated
+// by the registry
 ///////////////////////////////////////////////////////////////
 class Component {
 public:
-  Component();
-  Component(const Component &) = default;
-  ~Component();
+  Component() = default;
+  ~Component() = default;
+  Component(const Component&) = default;
+  int get_component_id() const;
 
 private:
-  
+  int component_id;
 };
+
+///////////////////////////////////////////////////////////////
+// Entity is an ID representing an object in the world 
 ///////////////////////////////////////////////////////////////
 class Entity {
 public:
-  Entity();
-  Entity(const Entity &) = default;
-  ~Entity();
+  Entity(int id) : entity_id{id} {}
+  ~Entity() = default;
+  Entity(const Entity&) = default;
+  int get_entity_id() const;
 
 private:
-  
+  int entity_id;
 };
+
+///////////////////////////////////////////////////////////////
+// System processes entities with a certain Signature
 ///////////////////////////////////////////////////////////////
 class System {
 public:
-  System();
-  System(const System &) = default;
-  ~System();
+  System() = default;
+  ~System() = default;
+  System(const System&) = default;
+  
+  void add_entity_to_system(Entity entity);
+  void remove_entity_from_system(Entity entity);
+  std::vector<Entity> get_system_entities() const;
+  Signature& get_component_signature() const;
 
 private:
-  
+  Signature component_signature;
+  std::vector<Entity> entities;
 };
+
+///////////////////////////////////////////////////////////////
+// The registry can manipulate an entity and its components
 ///////////////////////////////////////////////////////////////
 class Registry {
 public:
-  Registry();
-  Registry(const Registry &) = default;
-  ~Registry();
+  Registry() = default;
+  ~Registry() = default;
+  Registry(const Registry&) = default;
 
 private:
   
