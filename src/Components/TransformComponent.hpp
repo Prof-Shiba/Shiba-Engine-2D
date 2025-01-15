c22a85a53dd80831148552090e8a4acfa4f9ebf7
ProfShibe <166961473+ProfShibe@users.noreply.github.com>
2025-01-15 17:44:22 -0600

    Add TransformComponent constructor

--- src/Components/TransformComponent.hpp
+++ src/Components/TransformComponent.hpp
@@ -5,4 +5,8 @@ struct TransformComponent {
   glm::vec2 position;
   glm::vec2 scale;
   float rotation;
+
+  TransformComponent(glm::vec2 pos = glm::vec2(0, 0), glm::vec2 scale = glm::vec2(1, 1), float rot = 0.0) 
+    : position{pos}, scale{scale}, rotation{rot} {
+  }
 };
