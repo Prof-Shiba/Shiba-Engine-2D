3560c772ebd99c33296967a8c2f76a29eb7bc229
ProfShibe <166961473+ProfShibe@users.noreply.github.com>
2025-01-23 14:37:06 -0600

    Add MovementSystem with require_components and position logging

--- src/Systems/MovementSystem.hpp
+++ src/Systems/MovementSystem.hpp
@@ -1,11 +1,32 @@
 #pragma once
+#include "../ECS/ECS.hpp"
+#include "../Components/RigidBodyComponent.hpp"
+#include "../Components/TransformComponent.hpp"
+#include "../Game/Game.hpp"
 
 class MovementSystem : public System {
 public:
-  MovementSystem();
+  MovementSystem() {
+    require_component<TransformComponent>();
+    require_component<RigidBodyComponent>();
+  }
+
   MovementSystem(const MovementSystem&) = default;
-  ~MovementSystem();
-  void Update();
+  ~MovementSystem() = default;
+
+  void Update(double delta_time) {
+    for (auto entity : get_system_entities()) {
+      auto& transform = entity.get_component<TransformComponent>();
+      const auto rigid_body = entity.get_component<RigidBodyComponent>();
+
+      transform.position.x += rigid_body.velocity.x * delta_time;
+      transform.position.y += rigid_body.velocity.y * delta_time;
+
+      Logger::Log("Entity with ID [" + std::to_string(entity.get_entity_id()) + 
+                  "] is now at position x = [" + std::to_string(transform.position.x) + 
+                  "] and position y = " + std::to_string(transform.position.y) + "].");
+    }
+  }
 
 private:
   
