142dd0d3583319447d1582eccbe57e1cb816dc16
ProfShibe <166961473+ProfShibe@users.noreply.github.com>
2025-01-25 15:06:27 -0600

    Add asset_id arg to sprite component to know which asset we are rendering

--- src/Components/SpriteComponent.hpp
+++ src/Components/SpriteComponent.hpp
@@ -1,11 +1,13 @@
 #pragma once
 #include <cstdint>
+#include <string>
 
 struct SpriteComponent {
   uint16_t width;
   uint16_t height;
+  std::string asset_id;
 
-  SpriteComponent(uint16_t width = 0, uint16_t height = 0) 
-    : width{width}, height{height} {
+  SpriteComponent(std::string asset_id = "", uint16_t width = 0, uint16_t height = 0) 
+    : asset_id(asset_id), width{width}, height{height} {
   }
 };
