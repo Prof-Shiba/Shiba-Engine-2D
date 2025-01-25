673d886eb2f6659a7c2c9137814ce01d79c8a447
ProfShibe <166961473+ProfShibe@users.noreply.github.com>
2025-01-25 15:41:46 -0600

    Add src rect x and y to init list in constructor, also reordered init order to remove warnings

--- src/Components/SpriteComponent.hpp
+++ src/Components/SpriteComponent.hpp
@@ -1,13 +1,15 @@
 #pragma once
 #include <cstdint>
 #include <string>
+#include <SDL2/SDL.h>
 
 struct SpriteComponent {
   uint16_t width;
   uint16_t height;
   std::string asset_id;
+  SDL_Rect src_rect;
 
-  SpriteComponent(std::string asset_id = "", uint16_t width = 0, uint16_t height = 0) 
-    : asset_id(asset_id), width{width}, height{height} {
+  SpriteComponent(std::string asset_id = "", uint16_t width = 0, uint16_t height = 0, uint16_t src_rect_x = 0, int16_t src_rect_y = 0) 
+    :width{width}, height{height}, asset_id(asset_id), src_rect{src_rect_x, src_rect_y, width, height} {
   }
 };
