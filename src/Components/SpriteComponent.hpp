5246a51a7b7980d200bb0ddcce8b4b34f16f193c
ProfShibe <166961473+ProfShibe@users.noreply.github.com>
2025-01-30 17:04:46 -0600

    Update spacing

--- src/Components/SpriteComponent.hpp
+++ src/Components/SpriteComponent.hpp
@@ -11,5 +11,5 @@ struct SpriteComponent {
   SDL_Rect src_rect;
 
   SpriteComponent(std::string asset_id = "", uint16_t width = 0, uint16_t height = 0, uint16_t src_rect_x = 0, int16_t src_rect_y = 0, uint8_t z_index = 0)
-    :width{width}, height{height}, asset_id(asset_id), src_rect{src_rect_x, src_rect_y, width, height}, z_index{z_index} {}
+    : width{width}, height{height}, asset_id(asset_id), src_rect{src_rect_x, src_rect_y, width, height}, z_index{z_index} {}
 };
