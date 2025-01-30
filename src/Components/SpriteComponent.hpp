aa4492322c55f27d139c612b6e035a10bb875bf7
ProfShibe <166961473+ProfShibe@users.noreply.github.com>
2025-01-30 17:03:56 -0600

    Add z index variable for rendering layers

--- src/Components/SpriteComponent.hpp
+++ src/Components/SpriteComponent.hpp
@@ -6,10 +6,10 @@
 struct SpriteComponent {
   uint16_t width;
   uint16_t height;
+  uint8_t z_index;
   std::string asset_id;
   SDL_Rect src_rect;
 
-  SpriteComponent(std::string asset_id = "", uint16_t width = 0, uint16_t height = 0, uint16_t src_rect_x = 0, int16_t src_rect_y = 0) 
-    :width{width}, height{height}, asset_id(asset_id), src_rect{src_rect_x, src_rect_y, width, height} {
-  }
+  SpriteComponent(std::string asset_id = "", uint16_t width = 0, uint16_t height = 0, uint16_t src_rect_x = 0, int16_t src_rect_y = 0, uint8_t z_index = 0)
+    :width{width}, height{height}, asset_id(asset_id), src_rect{src_rect_x, src_rect_y, width, height}, z_index{z_index} {}
 };
