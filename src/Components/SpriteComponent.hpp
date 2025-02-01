f1bf2df5ea085d09527c66c307b306eda1029c5d
ProfShibe <166961473+ProfShibe@users.noreply.github.com>
2025-01-31 18:12:48 -0600

    Fix init order for SpriteComponent

--- src/Components/SpriteComponent.hpp
+++ src/Components/SpriteComponent.hpp
@@ -6,9 +6,9 @@
 struct SpriteComponent {
   uint16_t width;
   uint16_t height;
-  uint8_t z_index;
   std::string asset_id;
   SDL_Rect src_rect;
+  uint8_t z_index;
 
   SpriteComponent(std::string asset_id = "", uint16_t width = 0, uint16_t height = 0, uint16_t src_rect_x = 0, int16_t src_rect_y = 0, uint8_t z_index = 0)
     : width{width}, height{height}, asset_id(asset_id), src_rect{src_rect_x, src_rect_y, width, height}, z_index{z_index} {}
