078ccc789a65ade8fdb56cdd3873c3a62d44482d
ProfShibe <166961473+ProfShibe@users.noreply.github.com>
2025-02-01 14:18:18 -0600

    Update z_index data type to allow for negative z-indexes

--- src/Components/SpriteComponent.hpp
+++ src/Components/SpriteComponent.hpp
@@ -8,8 +8,8 @@ struct SpriteComponent {
   uint16_t height;
   std::string asset_id;
   SDL_Rect src_rect;
-  uint8_t z_index;
+  int8_t z_index;
 
-  SpriteComponent(std::string asset_id = "", uint16_t width = 0, uint16_t height = 0, uint16_t src_rect_x = 0, int16_t src_rect_y = 0, uint8_t z_index = 0)
+  SpriteComponent(std::string asset_id = "", uint16_t width = 0, uint16_t height = 0, uint16_t src_rect_x = 0, int16_t src_rect_y = 0, int8_t z_index = 0)
     : width{width}, height{height}, asset_id(asset_id), src_rect{src_rect_x, src_rect_y, width, height}, z_index{z_index} {}
 };
