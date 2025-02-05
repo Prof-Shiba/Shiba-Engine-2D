4cc510c3c123007b56156bcae3cb33390e4b47f0
ProfShibe <166961473+ProfShibe@users.noreply.github.com>
2025-02-05 15:21:33 -0600

    Add smelly but working AABB Collision Detection logic

--- src/Systems/CollisionSystem.hpp
+++ src/Systems/CollisionSystem.hpp
@@ -12,14 +12,27 @@ public:
   }
   ~CollisionSystem() = default;
 
+  // NOTE: Smelly
   void Update() {
     for (auto& entity: get_system_entities()) {
-      auto& collider = entity.get_component<BoxColliderComponent>();
-      auto& transform = entity.get_component<TransformComponent>();
+      if (entity.has_component<BoxColliderComponent>()) {
+        auto& collider = entity.get_component<BoxColliderComponent>();
+        auto& transform = entity.get_component<TransformComponent>();
 
-      // TODO: check if entity collider component is overlapping with another collider component
-      // then send a Logger Message!
+        for (auto& rhs: get_system_entities()) {
+          if (rhs.has_component<BoxColliderComponent>() && rhs.get_entity_id() != entity.get_entity_id()) {
+            auto& rhs_collider = rhs.get_component<BoxColliderComponent>();
+            auto& rhs_transform = rhs.get_component<TransformComponent>();
+
+          if(transform.position.x < rhs_transform.position.x + rhs_collider.width &&
+              transform.position.x + collider.width > rhs_transform.position.x &&
+              transform.position.y < rhs_transform.position.y + rhs_collider.height &&
+              transform.position.y + collider.height > rhs_transform.position.y)
+                Logger::Warn("Collision Detected!");
+          }
+        }
+      }
+
     }
-
   }
 };
