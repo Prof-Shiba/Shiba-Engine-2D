efb119e1aabfc22b82e57cf03a048dcf5abcb76f
ProfShibe <166961473+ProfShibe@users.noreply.github.com>
2025-02-16 13:46:19 -0600

    Add mostly functional damage system, seg faults attempting to remove entities directly

---
+++ src/Systems/DamageSystem.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "../ECS/ECS.hpp"
+#include "../Components/BoxColliderComponent.hpp"
+#include "../EventManager/EventManager.hpp"
+#include "../Events/CollisionEvent.hpp"
+#include "../Logger/Logger.hpp"
+#include <string>
+
+class DamageSystem: public System {
+public:
+  DamageSystem() {
+    require_component<BoxColliderComponent>();
+  }
+
+  void ListenForEvents(std::unique_ptr<EventManager>& event_manager) {
+    event_manager->listen_for_event(this, &DamageSystem::onCollision);
+  }
+
+  void onCollision(CollisionEvent& event) {
+    Logger::Log("DamageSystem event occured! Entities: " + std::to_string(event.lhs.get_entity_id()) + " and " + std::to_string(event.rhs.get_entity_id()) + "!");
+    /*event.lhs.remove();*/ // TODO: Fix seg faults
+    /*event.rhs.remove();*/
+  }
+
+  void Update() {
+    // TODO:
+  }
+};
