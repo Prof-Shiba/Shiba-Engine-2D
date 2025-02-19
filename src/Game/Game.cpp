0eedc318c73f4e8430704340b4b0be8e9474dca1
ProfShibe <166961473+ProfShibe@users.noreply.github.com>
2025-02-19 17:21:14 -0600

    Add comments for future to fix rendering issue while drawing tilemap

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -58,6 +58,9 @@ void Game::LoadLevel(int level) {
   uint8_t number_of_map_rows = 20;
   float tile_scale = 3.5;
 
+  // NOTE: Some race condition or smth similar here. Occasionally
+  // will not fully load the background, everything else always loads
+  // though
   std::ifstream in_file {"./assets/tilemaps/jungle.map"};
   if (in_file) {
     for (int y = 0; y < number_of_map_rows; y++) {
