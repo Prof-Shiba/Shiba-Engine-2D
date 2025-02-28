1c06e86c47ff481c73b7fccda0bfc04c5513881f
ProfShibe <166961473+ProfShibe@users.noreply.github.com>
2025-02-28 13:54:44 -0600

    Update KM sys to update rigid body velocity as well as sprite based on Key pressed

--- src/Systems/KeyboardMovementSystem.hpp
+++ src/Systems/KeyboardMovementSystem.hpp
@@ -3,10 +3,18 @@
 #include "../EventManager/EventManager.hpp"
 #include "../Events/KeyPressedEvent.hpp"
 #include "../Logger/Logger.hpp"
+#include "../Components/KeyboardControlComponent.hpp"
+#include "../Components/SpriteComponent.hpp"
+#include "../Components/RigidBodyComponent.hpp"
+#include <SDL2/SDL_keycode.h>
 
 class KeyboardMovementSystem : public System {
 public:
-  KeyboardMovementSystem() = default;
+  KeyboardMovementSystem() {
+    require_component<KeyboardControlComponent>();
+    require_component<SpriteComponent>();
+    require_component<RigidBodyComponent>();
+  };
   ~KeyboardMovementSystem() = default;
 
   void ListenForEvents(std::unique_ptr<EventManager>& event_manager) {
@@ -14,7 +22,32 @@ public:
   }
 
   void onKeyPressed(KeyPressedEvent& event) {
-    std::string pressed_key = SDL_GetKeyName(event.key_pressed);
-    Logger::Log("KeyPressed event occurred! Key: '" + pressed_key + "' was pressed!");
+    for (auto& entity: get_system_entities()) {
+      const auto& keyboard_control = entity.get_component<KeyboardControlComponent>();
+      auto& sprite = entity.get_component<SpriteComponent>();
+      auto& rigid_body = entity.get_component<RigidBodyComponent>();
+
+      switch (event.key_pressed) {
+        case SDLK_UP:
+          rigid_body.velocity = keyboard_control.up_velocity;
+          sprite.src_rect.y = sprite.height * 0;
+          break;
+
+        case SDLK_RIGHT:
+          rigid_body.velocity = keyboard_control.right_velocity;
+          sprite.src_rect.y = sprite.height * 1;
+          break;
+
+        case SDLK_DOWN:
+          rigid_body.velocity = keyboard_control.down_velocity;
+          sprite.src_rect.y = sprite.height * 2;
+          break;
+
+        case SDLK_LEFT:
+          rigid_body.velocity = keyboard_control.left_velocity;
+          sprite.src_rect.y = sprite.height * 3;
+          break;
+      }
+    }
   }
 };
