8ac620ca1fa6db354fccfa86e1ce13698134df4d
ProfShibe <166961473+ProfShibe@users.noreply.github.com>
2025-03-05 18:53:22 -0600

    Add collision components for helicopter, tank, and truck

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -15,6 +15,7 @@
 #include "../Components/AnimationComponent.hpp"
 #include "../Components/BoxColliderComponent.hpp"
 #include "../Components/KeyboardControlComponent.hpp"
+#include "../Components/CollisionComponent.hpp"
 #include "../Systems/MovementSystem.hpp"
 #include "../Systems/RenderSystem.hpp"
 #include "../Systems/AnimationSystem.hpp"
@@ -95,9 +96,9 @@ void Game::LoadLevel(int level) {
   helicopter.add_component<RigidBodyComponent>(glm::vec2(0.0, 0.0));
   helicopter.add_component<SpriteComponent>("helicopter-image", 32, 32, 0, 0, 3);
   helicopter.add_component<AnimationComponent>(2, 10, true);
-  helicopter.add_component<KeyboardControlComponent>(glm::vec2(0, -40), glm::vec2(40, 0), glm::vec2(0, 40), glm::vec2(-40, 0));
+  helicopter.add_component<KeyboardControlComponent>(glm::vec2(0, -80), glm::vec2(80, 0), glm::vec2(0, 80), glm::vec2(-80, 0));
   helicopter.add_component<BoxColliderComponent>(60, 60);
-  helicopter.is_colliding = true; // NOTE: does nothing
+  helicopter.add_component<CollisionComponent>();
 
   Entity radar = registry->create_entity();
   radar.add_component<TransformComponent>(glm::vec2(50, 100), glm::vec2(2.0, 2.0), 0.0);
@@ -110,13 +111,14 @@ void Game::LoadLevel(int level) {
   tank.add_component<RigidBodyComponent>(glm::vec2(50.0, 0.0));
   tank.add_component<SpriteComponent>("tank-image", 32, 32, 0, 0, 2); // imgs are 32px, width and height, src rect x, src rect y, then z-index
   tank.add_component<BoxColliderComponent>(60, 60);
-  tank.is_colliding = true; // NOTE: does nothing
+  tank.add_component<CollisionComponent>();
 
   Entity truck = registry->create_entity(); // 503
   truck.add_component<TransformComponent>(glm::vec2(300, 10), glm::vec2(2.0, 2.0), 0.0);
   truck.add_component<RigidBodyComponent>(glm::vec2(-50.0, 00.0));
   truck.add_component<SpriteComponent>("truck-image", 32, 32, 0, 0, 1);
   truck.add_component<BoxColliderComponent>(60, 50);
+  truck.add_component<CollisionComponent>();
 }
 
 void Game::Setup() {
