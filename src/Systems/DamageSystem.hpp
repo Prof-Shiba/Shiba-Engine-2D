48562f175af2c648661f92c5ed8fb8c59b5a7255
ProfShibe <166961473+ProfShibe@users.noreply.github.com>
2025-03-05 19:27:26 -0600

    Add working is_colliding detection in damage system. Must now be turned off

--- src/Systems/DamageSystem.hpp
+++ src/Systems/DamageSystem.hpp
@@ -2,6 +2,7 @@
 
 #include "../ECS/ECS.hpp"
 #include "../Components/BoxColliderComponent.hpp"
+#include "../Components/CollisionComponent.hpp"
 #include "../EventManager/EventManager.hpp"
 #include "../Events/CollisionEvent.hpp"
 #include "../Logger/Logger.hpp"
@@ -10,6 +11,7 @@ class DamageSystem: public System {
 public:
   DamageSystem() {
     require_component<BoxColliderComponent>();
+    require_component<CollisionComponent>();
   }
 
   void ListenForEvents(std::unique_ptr<EventManager>& event_manager) {
@@ -18,6 +20,8 @@ public:
 
   void onCollision(CollisionEvent& event) {
     Logger::Log("DamageSystem event occured! Entities: " + std::to_string(event.lhs.get_entity_id()) + " and " + std::to_string(event.rhs.get_entity_id()) + "!");
+    event.lhs.get_component<CollisionComponent>().is_colliding = true;
+    event.rhs.get_component<CollisionComponent>().is_colliding = true;
   }
 
   void Update() {
