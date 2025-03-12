83e3fea3fad17c98127b856b7d1f0e972647ca11
ProfShibe <166961473+ProfShibe@users.noreply.github.com>
2025-03-12 13:52:47 -0500

    Add ProjectileEmitterSystem to deal with projectile logic. Has TODO note for destroying projectiles

---
+++ src/Systems/ProjectileEmitterSystem.hpp
@@ -0,0 +1,49 @@
+#pragma once
+#include "../Components/ProjectileEmitterComponent.hpp"
+#include "../Components/TransformComponent.hpp"
+#include "../Components/RigidBodyComponent.hpp"
+#include "../Components/SpriteComponent.hpp"
+#include "../Components/BoxColliderComponent.hpp"
+#include "../Components/CollisionComponent.hpp"
+#include "../ECS/ECS.hpp"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_timer.h>
+
+// TODO: Destroy projectiles
+
+class ProjectileEmitterSystem : public System {
+public:
+  ProjectileEmitterSystem() {
+    require_component<ProjectileEmitterComponent>();
+    require_component<TransformComponent>();
+  }
+
+  void Update(const std::unique_ptr<Registry>& registry) {
+    for (auto& entity: get_system_entities()) {
+      auto& projectile_emitter = entity.get_component<ProjectileEmitterComponent>();
+      const auto& transform = entity.get_component<TransformComponent>();
+
+      if (SDL_GetTicks() - projectile_emitter.last_emission_time > projectile_emitter.repeat_speed) {
+        glm::vec2 projectile_pos = transform.position;
+
+        if (entity.has_component<SpriteComponent>()) {
+          const auto& sprite = entity.get_component<SpriteComponent>();
+          projectile_pos += (sprite.width / 2);
+          projectile_pos += (sprite.height / 2);
+        }
+
+        Entity projectile = registry->create_entity();
+        projectile.add_component<TransformComponent>(projectile_pos, glm::vec2(1.0, 1.0), 0);
+        projectile.add_component<RigidBodyComponent>(projectile_emitter.projectile_velocity);
+        projectile.add_component<SpriteComponent>("bullet-image", 4, 4, 0, 0, 3, false);
+        projectile.add_component<BoxColliderComponent>(4, 4);
+        projectile.add_component<CollisionComponent>();
+
+        projectile_emitter.last_emission_time = SDL_GetTicks();
+      }
+
+    }
+  }
+
+
+};
