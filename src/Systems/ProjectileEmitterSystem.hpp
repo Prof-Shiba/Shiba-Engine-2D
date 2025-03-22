9c254f506293fe7dc7135f12051d0e4d530e32fc
chilicheesedawg <166961473+chilicheesedawg@users.noreply.github.com>
2025-03-22 15:11:36 -0500

    Add working onKeyPressed projectile emission for player. Skip emissions with 0 repeat speed

--- src/Systems/ProjectileEmitterSystem.hpp
+++ src/Systems/ProjectileEmitterSystem.hpp
@@ -19,7 +19,6 @@ public:
     require_component<ProjectileEmitterComponent>();
     require_component<TransformComponent>();
   }
-  bool player_shooting = false;
 
   void ListenForEvents(std::unique_ptr<EventManager>& event_manager) {
     event_manager->listen_for_event(this, &ProjectileEmitterSystem::onKeyPressed);
@@ -27,18 +26,53 @@ public:
 
   void onKeyPressed(KeyPressedEvent& event) {
     if (event.key_pressed == SDLK_SPACE) {
-      player_shooting = true; 
+      for (auto& entity: get_system_entities()) {
+        if (entity.has_component<PlayerShootComponent>()) {
+          const auto& projectile_emitter = entity.get_component<ProjectileEmitterComponent>();
+          const auto& transform = entity.get_component<TransformComponent>();
+          const auto& rigid_body = entity.get_component<RigidBodyComponent>();
+
+          glm::vec2 projectile_pos = transform.position;
+
+          if (entity.has_component<SpriteComponent>()) {
+            auto& sprite = entity.get_component<SpriteComponent>();
+            projectile_pos.x += (static_cast<float>(sprite.width) / 2);
+            projectile_pos.y += (static_cast<float>(sprite.height) / 2);
+          }
+
+          glm::vec2 projectile_velocity = projectile_emitter.projectile_velocity;
+
+          int8_t x_direction = 0;
+          int8_t y_direction = 0;
+
+          if (rigid_body.velocity.x > 0) x_direction = +1;
+          if (rigid_body.velocity.x < 0) x_direction = -1;
+          if (rigid_body.velocity.y > 0) y_direction = +1;
+          if (rigid_body.velocity.y < 0) y_direction = -1;
+
+          projectile_velocity.x = projectile_emitter.projectile_velocity.x * x_direction;
+          projectile_velocity.y = projectile_emitter.projectile_velocity.y * y_direction;
+
+          Entity projectile = entity.registry->create_entity();
+          projectile.add_component<TransformComponent>(projectile_pos, glm::vec2(1.0, 1.0), 0);
+          projectile.add_component<RigidBodyComponent>(projectile_velocity);
+          projectile.add_component<SpriteComponent>("bullet-image", 4, 4, 0, 0, 3, false);
+          projectile.add_component<BoxColliderComponent>(4, 4);
+          projectile.add_component<CollisionComponent>();
+          projectile.add_component<ProjectileComponent>(projectile_emitter.is_friendly, projectile_emitter.damage, projectile_emitter.projectile_duration);
+          }
+      }
+
     }
   }
 
   void Update(const std::unique_ptr<Registry>& registry) {
     for (auto& entity: get_system_entities()) {
-      // FIXME: Bullets not at proper angle and loops through all entities before getting back to actual player, causing delay
-      if (entity.has_component<PlayerShootComponent>() && player_shooting == false)
-        continue;
-
       auto& projectile_emitter = entity.get_component<ProjectileEmitterComponent>();
       const auto& transform = entity.get_component<TransformComponent>();
+
+      if (projectile_emitter.repeat_speed == 0)
+        continue;
 
       if (SDL_GetTicks() - projectile_emitter.last_emission_time > projectile_emitter.repeat_speed) {
         glm::vec2 projectile_pos = transform.position;
@@ -58,9 +92,6 @@ public:
         projectile.add_component<ProjectileComponent>(projectile_emitter.is_friendly, projectile_emitter.damage, projectile_emitter.projectile_duration);
 
         projectile_emitter.last_emission_time = SDL_GetTicks();
-
-        if (entity.has_component<PlayerShootComponent>())
-          player_shooting = false;
       }
     }
   }
