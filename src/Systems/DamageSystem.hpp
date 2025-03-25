82fd4b3a90276e4411f26b0eefc38bdb405c31c8
chilicheesedawg <166961473+chilicheesedawg@users.noreply.github.com>
2025-03-25 16:40:50 -0500

    Add logic for collision of projectiles and enemies/player

--- src/Systems/DamageSystem.hpp
+++ src/Systems/DamageSystem.hpp
@@ -3,6 +3,8 @@
 #include "../ECS/ECS.hpp"
 #include "../Components/BoxColliderComponent.hpp"
 #include "../Components/CollisionComponent.hpp"
+#include "../Components/ProjectileComponent.hpp"
+#include "../Components/HealthComponent.hpp"
 #include "../EventManager/EventManager.hpp"
 #include "../Events/CollisionEvent.hpp"
 #include "../Logger/Logger.hpp"
@@ -20,8 +22,51 @@ public:
 
   void onCollision(CollisionEvent& event) {
     Logger::Log("DamageSystem event occured! Entities: " + std::to_string(event.lhs.get_entity_id()) + " and " + std::to_string(event.rhs.get_entity_id()) + "!");
+
     event.lhs.get_component<CollisionComponent>().is_colliding = true;
     event.rhs.get_component<CollisionComponent>().is_colliding = true;
+
+    if ( (event.lhs.belongs_to_group("projectile") && event.rhs.has_tag("player")) || (event.lhs.has_tag("player") && event.rhs.belongs_to_group("projectile")) ) 
+      (event.lhs.belongs_to_group("projectile")) ? Projectile_hit_player(event.lhs, event.rhs) : Projectile_hit_player(event.rhs, event.lhs);
+
+    else if ( (event.lhs.belongs_to_group("projectile") && event.rhs.belongs_to_group("enemy")) || (event.lhs.belongs_to_group("enemy") && event.rhs.belongs_to_group("projectile")) )
+      (event.lhs.belongs_to_group("projectile")) ? Projectile_hit_enemy(event.lhs, event.rhs) : Projectile_hit_enemy(event.rhs, event.lhs);
+    else
+     return;
+  }
+
+  void Projectile_hit_player(Entity& projectile, Entity& player) {
+    Logger::Log("Projectile hit player!");
+
+    auto& projectile_component = projectile.get_component<ProjectileComponent>();
+    auto& health_component = player.get_component<HealthComponent>();
+
+    if (!projectile_component.is_friendly) {
+      health_component.health_amount -= projectile_component.damage;
+      projectile.remove();
+    }
+
+    if (health_component.health_amount <= 0) {
+      Logger::Log("Player with ID " + std::to_string(player.get_entity_id()) + " was killed!");
+      player.remove();
+    }
+
+  }
+  void Projectile_hit_enemy(Entity& projectile, Entity& enemy) {
+    Logger::Log("Projectile hit enemy!");
+
+    auto& projectile_component = projectile.get_component<ProjectileComponent>();
+    auto& health_component = enemy.get_component<HealthComponent>();
+
+    if (projectile_component.is_friendly) {
+      health_component.health_amount -= projectile_component.damage;
+      projectile.remove();
+    }
+
+    if (health_component.health_amount <= 0) {
+      Logger::Log("Enemy with ID " + std::to_string(enemy.get_entity_id()) + " was killed!");
+      enemy.remove();
+    }
   }
 
   void Update() {
