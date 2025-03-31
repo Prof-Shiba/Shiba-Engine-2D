f9cffcccd3c563f523783211c8c96f40dad71bc1
ProfShibe <166961473+ProfShibe@users.noreply.github.com>
2025-03-31 15:08:22 -0500

    Add function declaration for adding and getting fonts

--- src/AssetManager/AssetManager.hpp
+++ src/AssetManager/AssetManager.hpp
@@ -3,18 +3,23 @@
 #include <map>
 #include <string>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 
 class AssetManager {
-  public:
+public:
   AssetManager();
   ~AssetManager();
 
   void clear_assets();
+
   void add_texture(SDL_Renderer* renderer, const std::string& asset_id, const std::string& file_path);
   SDL_Texture* get_texture(const std::string& asset_id) const;
 
-  private:
-    std::map<std::string, SDL_Texture*> textures;
-    // TODO: Fonts map  
-    // TODO: Audio map  
+  void add_font(const std::string& asset_id, const std::string& file_path, uint16_t font_size);
+  TTF_Font* get_font(const std::string& asset_id);
+
+private:
+  std::map<std::string, SDL_Texture*> textures;
+  std::map<std::string, TTF_Font*> fonts;
+  // TODO: Audio map  
 };
