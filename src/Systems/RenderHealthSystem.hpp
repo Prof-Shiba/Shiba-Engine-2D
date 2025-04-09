8f28a7ee6476b68fe059d55b97877086b352f014
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-04-09 18:38:25 -0500

    Change bar to decrease in size as health lowers. Tidy up code

--- src/Systems/RenderHealthSystem.hpp
+++ src/Systems/RenderHealthSystem.hpp
@@ -17,30 +17,36 @@ public:
     for (auto& entity: get_system_entities()) {
       const auto& health = entity.get_component<HealthComponent>();
       const auto& transform = entity.get_component<TransformComponent>();
-      const uint16_t WIDTH = 60;
-      const uint16_t HEIGHT = 5;
       const uint16_t Y_OFFSET = 65;
+      const uint16_t HEIGHT = 5;
+      int16_t width = 60;
+
+      // NOTE: Running into this while loop
+      // every single time! Find a better way
+      // of doing this! 2025-04-09
+      if (health.health_amount != 100) {
+        uint16_t missing_health = 100 - health.health_amount;
+        while (missing_health != 0) {
+          width -= 6;
+          missing_health -= 10;
+        }
+      }
 
       SDL_Rect rect {
         static_cast<int>(transform.position.x - camera.x),
         static_cast<int>(transform.position.y - camera.y) + Y_OFFSET,
-        WIDTH,
+        width,
         HEIGHT
       };
 
-      if (health.health_amount >= 71) {
+      if (health.health_amount >= 71)
         SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-        SDL_RenderFillRect(renderer, &rect);
-      }
-      else if (health.health_amount <= 70 && health.health_amount >= 41) {
+      else if (health.health_amount <= 70 && health.health_amount >= 31)
         SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
-        SDL_RenderFillRect(renderer, &rect);
-      }
-      else {
+      else 
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-        SDL_RenderFillRect(renderer, &rect);
-      }
 
+      SDL_RenderFillRect(renderer, &rect);
       SDL_RenderDrawRect(renderer, &rect);
     }
   }
