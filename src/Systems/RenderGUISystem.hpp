b2d92671f21210806197f5f766ddad70f2726aba
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-04-14 15:36:50 -0500

    Add new ImGui features like changing sprite, health, godmode, and some spacing to look prettier

--- src/Systems/RenderGUISystem.hpp
+++ src/Systems/RenderGUISystem.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <SDL2/SDL_render.h>
+#include <array>
 #include "../ECS/ECS.hpp"
 #include "../../libs/imgui/imgui.h"
 #include "../../libs/imgui/backends/imgui_impl_sdl2.h"
@@ -25,33 +26,47 @@ public:
     ImGui::NewFrame();
 
     if (ImGui::Begin("Spawn Enemies")) {
+
+      ImGui::SeparatorText("Position");
       static int32_t enemy_x_pos = 0;
       static int32_t enemy_y_pos = 0;
-      static int32_t enemy_health = 100;
-      // static bool enemy_godmode = false;
-
       ImGui::InputInt("Enemy x", &enemy_x_pos);
       ImGui::InputInt("Enemy y", &enemy_y_pos);
+
+      ImGui::SeparatorText("Health");
+      static int32_t enemy_health = 100;
+      static bool enemy_godmode = false;
+      ImGui::SliderInt("Enemy health", &enemy_health, 1, 100);
+      ImGui::Checkbox("Enemy Godmode", &enemy_godmode);
+
+      const static char* sprites[] = { "tank-image", "truck-image"};
+      static int32_t current_sprite = 0;
+      static std::string enemy_name = "";
+      (current_sprite == 0) ? enemy_name = "Tank" : enemy_name = "Truck";
+      ImGui::SeparatorText("Sprite image");
+      ImGui::Combo("Sprite selection", &current_sprite, sprites, 2);
 
       // TODO: Everything else besides pos
       // Scale, rotation.
       // Velocity for enemy
-      // Dropdown list (combo box) for sprite texture ID
       // Angle and speed of projectiles
       // Repeat freq and duration of projectiles (in seconds)
 
+      // creates invis rect for space
+      ImGui::Dummy(ImVec2(0, 35));
+      
       if (ImGui::Button("Spawn enemy")) {
         Entity new_enemy = registry->create_entity();
         new_enemy.group("enemy");
         new_enemy.add_component<TransformComponent>(glm::vec2(enemy_x_pos, enemy_y_pos), glm::vec2(2.0, 2.0), 0.0);
         new_enemy.add_component<RigidBodyComponent>(glm::vec2(0.0, 0.0));
-        new_enemy.add_component<SpriteComponent>("tank-image", 32, 32, 0, 0, 2);
+        new_enemy.add_component<SpriteComponent>(sprites[current_sprite], 32, 32, 0, 0, 2);
         new_enemy.add_component<BoxColliderComponent>(60, 60);
         new_enemy.add_component<CollisionComponent>();
         new_enemy.add_component<HealthComponent>(enemy_health);
         new_enemy.add_component<ProjectileEmitterComponent>(glm::vec2(100, 0), 5000, 10000, 10, false);
-        new_enemy.add_component<GodModeComponent>(false);
-        new_enemy.add_component<MovingTextComponent>(7, -10, "Tank", "arial-font", SDL_Color {255, 0, 0});
+        new_enemy.add_component<GodModeComponent>(enemy_godmode);
+        new_enemy.add_component<MovingTextComponent>(7, -10, enemy_name.c_str(), "arial-font", SDL_Color {255, 0, 0});
       }
     }
     ImGui::End();
