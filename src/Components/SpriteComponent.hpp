4cb269c423acf59744e2764fb259983d71de5a33
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-04-16 15:46:17 -0500

    Add SDL_RendererFlip to sprite component for flipping entities on obj collision

--- src/Components/SpriteComponent.hpp
+++ src/Components/SpriteComponent.hpp
@@ -10,7 +10,8 @@ struct SpriteComponent {
   SDL_Rect src_rect;
   int8_t z_index;
   bool is_fixed;
+  SDL_RendererFlip flip;
 
   SpriteComponent(std::string asset_id = "", uint16_t width = 0, uint16_t height = 0, uint16_t src_rect_x = 0, int16_t src_rect_y = 0, int8_t z_index = 0, bool is_fixed = false)
-    : asset_id(asset_id), width{width}, height{height}, src_rect{src_rect_x, src_rect_y, width, height}, z_index{z_index}, is_fixed{is_fixed} {}
+    : asset_id(asset_id), width{width}, height{height}, src_rect{src_rect_x, src_rect_y, width, height}, z_index{z_index}, is_fixed{is_fixed}, flip{SDL_FLIP_NONE} {}
 };
