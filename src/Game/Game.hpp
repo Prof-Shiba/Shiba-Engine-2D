e158e19c36e02fdd49409ff395bf43e17cc8925f
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-04-18 12:52:42 -0500

    Add sol::state lua to header to manage a single instance and change some ordering of vars

--- src/Game/Game.hpp
+++ src/Game/Game.hpp
@@ -7,11 +7,12 @@
 #include "../ECS/ECS.hpp"
 #include "../AssetManager/AssetManager.hpp"
 #include "../EventManager/EventManager.hpp"
+#include "../../libs/sol/sol.hpp"
 
+const int8_t DEFAULT_MONITOR_NUMBER = -1;
 const uint16_t TARGET_FPS = 144;
 // 1000ms -> 1 second. Each frame should take 16.6 repeating ms
 const uint16_t MS_PER_FRAME = 1000 / TARGET_FPS;
-const int8_t DEFAULT_MONITOR_NUMBER = -1;
 
 class Game {
 public:
@@ -33,12 +34,13 @@ public:
   bool debug_enabled;
 
 private:
+  uint16_t current_fps;
+  uint32_t ms_previous_frame = 0;
+  sol::state lua;
   SDL_Window* window;
   SDL_Renderer* renderer;
   SDL_Rect camera;
-  uint32_t ms_previous_frame = 0;
   std::unique_ptr<Registry> registry;
   std::unique_ptr<AssetManager> asset_manager;
   std::unique_ptr<EventManager> event_manager;
-  uint16_t current_fps;
 };
