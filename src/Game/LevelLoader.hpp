8f13dae3177ad0a27a106c493d2144a60c9c0a06
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-04-18 12:53:05 -0500

    Add sol::state lua to level loader header

--- src/Game/LevelLoader.hpp
+++ src/Game/LevelLoader.hpp
@@ -4,11 +4,12 @@
 #include "../ECS/ECS.hpp"
 #include "../AssetManager/AssetManager.hpp"
 #include <SDL2/SDL_render.h>
+#include "../../libs/sol/sol.hpp"
 
 class LevelLoader {
 public:
   LevelLoader();
   ~LevelLoader();
 
-  void LoadLevel(uint8_t level, const std::unique_ptr<Registry>& registry, const std::unique_ptr<AssetManager>& asset_manager, SDL_Renderer* renderer);
+  void LoadLevel(sol::state& lua, uint8_t level, const std::unique_ptr<Registry>& registry, const std::unique_ptr<AssetManager>& asset_manager, SDL_Renderer* renderer);
 };
